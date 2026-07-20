959570222b62805c916d1ce47ec17caa67b15d2c
Luis Alvarenga <l.alvarengav@gmail.com>
2026-07-20 00:18:20 -0600

    return and handle errors

--- greetings.go
+++ greetings.go
@@ -1,10 +1,18 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
+// Hello return a greeting for the named person
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	// Return a greeting that embeds the name in message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 
-	return message
+	return message, nil
 }
