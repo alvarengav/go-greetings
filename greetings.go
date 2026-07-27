620c88c548c75e06bcb72bba67fffc02117bef9a
Luis Alvarenga <l.alvarengav@gmail.com>
2026-07-26 18:58:54 -0600

    return a random greetings

--- greetings.go
+++ greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 // Hello return a greeting for the named person
@@ -12,7 +13,22 @@ func Hello(name string) (string, error) {
 		return "", errors.New("empty name")
 	}
 	// Return a greeting that embeds the name in message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(),name)
 
 	return message, nil
+}
+
+// radomFormat returns one of a set of greeting messsage. The returned
+// message is selected at random.
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string {
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
